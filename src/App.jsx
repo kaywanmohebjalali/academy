cb92798fa8d7a9a686074abf0effb17d692472bd
kaywanmohebjalali <mohebjalali1003@gmail.com>
2024-02-18 16:16:37 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+import GlobalStyles from "./styles/GlobalStyles"
+
+
+const StyleApp=styled.div`
+  
+  background-color: red;
+  padding: 2rem;
+  color: #fff;
+`
+
+const App = () => {
+  return (
+    <>
+    <GlobalStyles/>
+    <StyleApp>App</StyleApp>
+    </>
+  )
+}
+
+export default App
